6f77d804046f84fec19fabde90878b19d1dab223
Nguyen Ha <ngocha.mac@gmail.com>
2021-07-03 01:36:58 +0900

    update My Page

--- MyPage/app.js
+++ MyPage/app.js
@@ -7,12 +7,26 @@ function getLoginUsers () {
     console.log(loginUsers);
 }
 function showUsername(){
-    getLoginUsers()
+    let loginUserStr = localStorage.getItem('loginUsers');
+    let loginUsers = JSON.parse(loginUserStr);
     usernameMypage.innerHTML = loginUsers[0].username;
     usernameMyaccount.innerHTML = "Username: " + loginUsers[0].username;
 }
 showUsername();
 
+//Show email address
+//get loginUser, get user, tìm index của object user có username trùng với login user, hiển thị email address 
+let emailMyaccount = document.getElementById('myaccount-email')
+function showEmail(){
+    let userStr = localStorage.getItem('users');
+    let users = JSON.parse(userStr);
+    let loginUserStr = localStorage.getItem('loginUsers');
+    let loginUsers = JSON.parse(loginUserStr);
+    let index = users.findIndex(x => x.username === loginUsers[0].username);
+    console.log(index);
+    emailMyaccount.innerHTML = "Email address: " + users[index].email;
+}
+showEmail();
 //Logout
 let logoutBtn = document.getElementById('logout-btn');
 logoutBtn.addEventListener('click', logout);
@@ -74,17 +88,17 @@ function closeChangeUserModal(){
 //Change user name
 let newUsername = document.getElementById('change-username');
 let cfUsername = document.getElementById('confirm-username');
+let newUserErr = document.getElementById('new-username-err');
+let confirmUserErr = document.getElementById('confirm-username-err');
 function validateUsername() {
     if (newUsername.value == "") {
-        alert('Please input username');
+        newUserErr.innerHTML = 'Please input username';
         return false
-    }
-    if (cfUsername.value == "") {
-        alert('Please input confirm username');
+    } else if (cfUsername.value == "") {
+        confirmUserErr.innerHTML ='Please input confirm username';
         return false
-    } 
-    if (newUsername.value != cfUsername.value) {
-        alert('Wrong confirm username');
+    } else if (newUsername.value != cfUsername.value) {
+        confirmUserErr.innerHTML = 'Wrong confirm username';
         return false
     } else {
         return true
@@ -96,13 +110,21 @@ function getUsers() {
     console.log(users);
 }
 function changeUsername() {
-    //get users từ local storage
-    getUsers()
-    getLoginUsers();
+    //get users và loginUsers từ local storage
+    let userStr = localStorage.getItem('users');
+    let users = JSON.parse(userStr);
+    let loginUserStr = localStorage.getItem('loginUsers');
+    let loginUsers = JSON.parse(loginUserStr);
+
     let index = users.findIndex(x => x.username === loginUsers[0].username);
+    console.log(index);
     users[index].username = newUsername.value;
     loginUsers[0].username = newUsername.value;
+    //lưu vào local storage
+    localStorage.setItem('users', JSON.stringify(users));
+    localStorage.setItem('loginUsers', JSON.stringify(loginUsers));
     showUsername();
+    closeChangeUserModal();
 }
 function changeUser() {
     validateUsername();
@@ -112,3 +134,4 @@ function changeUser() {
 }
 let submitBtn = document.getElementById('submit-change');
 submitBtn.addEventListener('click', changeUser);
+
